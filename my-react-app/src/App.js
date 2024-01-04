f5f2ba8a2a8aae6e77f85ad36135e206d5e80ac2
samuel21695 <samuel21695@gmail.com>
2024-01-04 23:11:32 +0900

    ✨ useState component example

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Counter from './components/UseState';
 
 function App() {
   return (
     <div className="App">
-      test
+      <Counter />
     </div>
   );
 }
--- my-react-app/src/components/UseState.js
+++ my-react-app/src/components/UseState.js
@@ -12,4 +12,4 @@ function Counter() {
   );
 }
 
-Counter()
+export default Counter
