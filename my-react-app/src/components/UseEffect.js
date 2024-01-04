f4adbfe50d970c155df516b2e9a7acec3866e875
samuel21695 <samuel21695@gmail.com>
2024-01-05 00:36:47 +0900

    ✨ useEffect-2

--- my-react-app/src/components/UseEffect.js
+++ my-react-app/src/components/UseEffect.js
@@ -1,13 +1,20 @@
 import React, {useEffect, useState} from 'react';
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
-    // 컴포넌트가 렌더링될 때마다 실행되는 함수
-  }, []);
+    // 컴포넌트가 렌더링될 때마다 count의 값을 localStorage에 저장합니다.
+    localStorage.setItem("count", count);
+  }, [count]);
 
   return (
     <div>
-      {/* ... */}
+      <h1>카운터</h1>
+      <p>현재 값: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   );
 }
+
+export default Counter
