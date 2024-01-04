c5a9adab5f0f5a8b8351b2e0deed3eaa928faed6
samuel21695 <samuel21695@gmail.com>
2024-01-05 00:35:55 +0900

    ✨useEffect -1

---
+++ my-react-app/src/components/UseEffect.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+
+function App() {
+  useEffect(() => {
+    // 컴포넌트가 렌더링될 때마다 실행되는 함수
+  }, []);
+
+  return (
+    <div>
+      {/* ... */}
+    </div>
+  );
+}
